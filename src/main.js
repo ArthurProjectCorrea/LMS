445184d18816407d7d50c2ca68d616d7fa4b97da
Arthur de Paula Correa <arthurdepaulacorrea@hotmail.com>
2024-09-05 22:36:32 -0300

    Install: FontAwesome library

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import './assets/main.css'
-import './assets/tailwind.css'
+import "./assets/main.css";
+import "./assets/tailwind.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 
-app.use(router)
+library.add(faUserSecret);
 
-app.mount('#app')
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
+  .mount("#app");
