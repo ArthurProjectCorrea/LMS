02e0cee577544065832e7be796073927db0387e1
Arthur de Paula Correa <arthurdepaulacorrea@hotmail.com>
2024-09-05 22:59:53 -0300

    Install: FontAwesome library

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
-import { faUserSecret } from "@fortawesome/free-solid-svg-icons"
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faUserSecret);
 
